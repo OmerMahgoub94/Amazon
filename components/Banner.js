726da8710ed1771d9a728363e3c7eee09747b568
unknown <omer.mhjoub@gmail.com>
2021-08-10 21:00:25 +0300

    deploy6

--- components/Banner.js
+++ components/Banner.js
@@ -1,5 +1,5 @@
 import { Carousel } from 'react-responsive-carousel'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/Carousel.css"; // requires a loader
 
 function Banner() {
     return (
