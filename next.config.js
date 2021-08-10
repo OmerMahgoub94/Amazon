66c287d47af8ba69ddfeb9a26a9203b46510aa9b
unknown <omer.mhjoub@gmail.com>
2021-08-10 21:49:26 +0300

    a

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ module.exports = {
     images: {
         domains: ['links.papareact.com', 'fakestoreapi.com']
     },
-    rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-    ]
-
-
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css';
-<link rel="stylesheet" href="carousel.css" />
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
