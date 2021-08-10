3652b64e7bfcc5f9a28d5c990fff4f1d621b3492
unknown <omer.mhjoub@gmail.com>
2021-08-10 20:40:45 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com', 'fakestoreapi.com']
-    }
+    },
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
 }
